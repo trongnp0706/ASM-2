f1313a7bf927dfeadbd25e0b6757f3f661f9bcca
Nguyen Phuoc Trong <phuoctrong1001@gmail.com>
2022-11-09 02:31:23 +0700

    add role and show admin page

--- routes/login.js
+++ routes/login.js
@@ -11,19 +11,25 @@ router.get('/', function(req, res, next) {
 router.post('/', async function(req, res, next) {
 let username = req.body.username;
 let password = req.body.password;
-let [authenticated, shop_id] = await authen(username, password)
+let [authenticated, shop_id, role] = await authen(username, password)
 console.log(authenticated, shop_id)
-if (authenticated == true) {
+if (authenticated == true & role == 'shop') {
   var table = await getTable(shop_id)
 
   res.render('users', { title: 'Welcome to ATN SHOP',
                         name: username,
                         table_string: table});
 }
-
+else if (authenticated == true & role == 'admin') {
+  res.render('admin', {
+    title: 'Welcome to Admin page of ATN-SHOP',
+    name: username,
+    // table_string: table,
+  });
+}
 else {
   res.render('login', { title: 'ATN SHOP',
-                        message: 'Wrong username or password'});
+                        message: 'Wrong username or password. Please try again!'});
 }
 });
 
